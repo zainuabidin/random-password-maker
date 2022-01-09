6df378d799c83bf48e10f3d7ace23f8a1008cdd1
Zain Abidin <xan.abdn@gmail.com>
2022-01-09 23:36:55 +0000

    added variables for chracter choice

--- index.js
+++ index.js
@@ -1,93 +1,58 @@
-
-  // variablers been declared using assignment operators
-  var numeric ="0123456789";
-  var symbols="!@#$%^&*()_+~\\`|}{[]:;?><,./-=";
-  var lowerCase="abcdefghijklmnopqrstuvwxyz";
-  var upperCase="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var passGen='';
-
+// variablers been declared using assignment operators
+// var numeric ="0123456789";
+// var symbols="!@#$%^&*()_+~\\`|}{[]:;?><,./-=";
+// var lowerCase="abcdefghijklmnopqrstuvwxyz";
+// var upperCase="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+// var passGen='';
 
 // // This function invoke when click me button password on webpage
 function passwordCreator() {
-  var passGen=''
-  var passwordChoice =prompt
-    ("please choose a password length of at least 8 characters and no more than 128 characters"
+  // var passGen=''
+  // var numeric ="0123456789";
+  // var symbols="!@#$%^&*()_+~\\`|}{[]:;?><,./-=";
+  // var lowerCase="abcdefghijklmnopqrstuvwxyz";
+  // var upperCase="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var passwordChoice = prompt(
+    "please choose a password length of at least 8 characters and no more than 128 characters"
   );
-// conditional statments with two  branchers to validate the password is a number ,<8 and >128 and not blank
-  if (passwordChoice > 8 && passwordChoice < 128) {
-    alert("☺️ Thanks for confirming the password length ✅ ✅ \n Please press OK to proceed ")
-  } 
-  else if ((passwordChoice = isNaN || passwordChoice !== Number)) {
-    alert("    😔 Something does not seems right ❌ \n ☑ Please check the password lenght is correct \n ☑ you enter the valid number");
-  
-return;}
-
-  // conditional statements for user to choose character type  
-  var numeric = confirm("Would you like to add numerics in your password")
-  if(numeric){
-    passGen=passGen+numeric
-    
-   }
-
-  var lowerCase = confirm("Would you like to add lowerCase in your password")
-  if(lowerCase){
-    passGen=passGen+lowerCase
-    
-   }
-  var upperCase = confirm("Would you like to add upperCase in your password")
-  if(upperCase){
-    passGen=passGen+upperCase
-    
-   }
-  var symbols = confirm("Would you like to add symbols in your password")
-  if(symbols){
-    passGen=passGen+symbols
-    
-   }
-  
-  if (numeric==false&& lowerCase==false && upperCase==false&&symbols== false) {
-    
-    alert("please select at least one character type");
+  // conditional statements with two  branches to validate the password is a number ,<8 and >128 and not blank
+  if (passwordChoice > 7 && passwordChoice < 129) {
+    alert(
+      "☺️ Thanks for confirming the password length ✅ ✅ \n Please press OK to proceed "
+    );
+  } else if ((passwordChoice = isNaN || passwordChoice !== Number)) {
+    alert(
+      "    😔 Something does not seems right ❌ \n ☑ Please check the password length is correct \n ☑ you enter the valid number"
+    );
+    console.log(passwordChoice);
     return;
   }
 
+  // conditional statements for user to choose character type
+  var passGen = "";
 
+  var numeric = confirm(
+    "Would you like to add lowerCase in your password\npress ok to add and cancel to ignore"
+  );
+  if (numeric == 1) {
+    passGen = passGen + "0123456789";
   }
-  
-  
-// for (let i = 0; i < numeric.length; i++) {
-//   text += numeric[i] + "<br>";
-// }
 
-// document.getElementById("demo").innerHTML = numeric;
-// }
+  var lowerCase = confirm("Would you like to add lowerCase in your password");
+  if (lowerCase == 1) {
+    passGen = passGen + "abcdefghijklmnopqrstuvwxyz";
+  }
+  var upperCase = confirm("Would you like to add upperCase in your password");
+  if ((upperCase = 1)) {
+    passGen = passGen + "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  }
+  var symbols = confirm("Would you like to add symbols in your password");
+  if (symbols == 1) {
+    passGen = passGen + "!@#$%^&*()_+~\\`|}{[]:;?><,./-=";
+    console.log(passGen);
+  }
 
-
-
-
-//using conditional statements to obtain user password choice
-
-
-
-
-
-
-
-
-
-
-// //   GIVEN I need a new, secure password
-// WHEN I click the button to generate a password
-// THEN I am presented with a series of prompts for password criteria
-// WHEN prompted for password criteria
-// THEN I select which criteria to include in the password
-// WHEN prompted for the length of the password
-// THEN I choose a length of at least 8 characters and no more than 128 characters
-// WHEN asked for character types to include in the password
-// THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-// WHEN I answer each prompt
-// THEN my input should be validated and at least one character type should be selected
-// WHEN all prompts are answered
-// THEN a password is generated that matches the selected criteria
-// WHEN the password is generated
-// THEN the password is either displayed in an alert or written to the page
+  if (numeric == 0 && lowerCase == 0 && upperCase == 0 && symbols == 0) {
+    alert("You must select at least one character type");
+  }
+}
